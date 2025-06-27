be6c4e5ea07a65513f01aeb216fbf1cc318edab6
Altroo <scorpion.yooy@gmail.com>
2025-06-27 11:01:51 +0100

    initial commit

--- urlchecker/settings.py
+++ urlchecker/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 
 # Static files finders
 STATICFILES_FINDERS = [
