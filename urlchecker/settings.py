03531e71a8503b5074cc6de969ac855878419c88
Altroo <scorpion.yooy@gmail.com>
2025-06-27 11:28:57 +0100

    initial commit

--- urlchecker/settings.py
+++ urlchecker/settings.py
@@ -128,13 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 
-# Static files finders
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
